a6937b3fa66066086b3980f823b8f59ec838b7e3
TinyHust <tiny.hust.lee@gmail.com>
2022-09-03 11:35:57 +0700

    :cow: 11.35.03.09.22

--- examples/microservices/orders/src/index.ts
+++ examples/microservices/orders/src/index.ts
@@ -46,7 +46,7 @@ const start = async () => {
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-    console.log("Connected to MongoDBB11");
+    console.log("Connected to MongoDb");
   } catch (err) {
     console.error(err);
   }
